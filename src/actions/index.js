efb98d24b728ffb6dec7887f90fb08e53ed18b24
Peter Wang <happypeter1983@gmail.com>
2017-12-21 14:53:13 +0800

    call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,15 @@
 import axios from 'axios'
 import { POSTS_URL } from '../constants/ApiConstants'
 import * as types from '../constants/ActionTypes'
+import { call } from 'redux-saga/effects'
+
+const api = url => axios.get(url).then(res => res.data)
 
 export const fetchPostsRequest = () => ({
   type: types.FETCH_POSTS_REQUEST
 })
 
-export const fetchPosts = () => {
-  console.log('fetchPosts...这里来执行异步操作')
+export function * fetchPosts () {
+  const posts = yield call(api, POSTS_URL)
+  console.log(posts)
 }
